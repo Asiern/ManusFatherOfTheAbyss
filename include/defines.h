2d36f56f25fcdbd73e15664efb79711d2d2ac6bb
asiern <asnulorri@gmail.com>
2022-04-28 10:14:49 +0200

    update: baudrate

--- include/defines.h
+++ include/defines.h
@@ -42,7 +42,7 @@
 
 #define Fosc 40000000                       // Frecuencia de reloj de la CPU (oscillator)
 #define Fcy Fosc / 2                        // Velocidad de ejecucion de las instrucciones
-#define BAUDRATE2 9600                      // Velocidad de transmision de UART2 (baudios)
+#define BAUDRATE2 19200                     // Velocidad de transmision de UART2 (baudios)
 #define T_1BIT_US (1000000 / BAUDRATE2) + 1 // Duracion de 1 bit
 
 #define BAUD_RATEREG_2_BRGH1 ((Fcy / BAUDRATE2) / 4) - 1  // valor U2BRG si BRGH=1
