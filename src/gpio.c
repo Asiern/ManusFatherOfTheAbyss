ed9ca1582354668bd318700245151e45b27c3ea6
Asiern <asnulorri@gmail.com>
2022-05-03 12:08:47 +0200

    fix: inicPuertos

--- src/gpio.c
+++ src/gpio.c
@@ -11,10 +11,9 @@ void inicPulsadores()
 
 void inicPuertos()
 {
-    // Definir pines AN16 - AN31 como pines digitales para evitar problemas
-    AD1PCFGH = 0xffff;
-
-    TRISDbits.TRISD6 = 1; // Definir los pines del pulsador S3 como entrada
+    TRISBbits.TRISB0 = 1;
+    TRISBbits.TRISB1 = 1;
+    TRISBbits.TRISB2 = 1;
     TRISBbits.TRISB8 = 1;
     TRISBbits.TRISB9 = 1;
     TRISBbits.TRISB10 = 1;
